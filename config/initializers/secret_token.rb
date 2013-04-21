3b30f834db627ccd15947bf40f248e2f65f5d231
Gerardino Illescas <gerardo586x@gmail.com>
2013-04-21 00:15:23 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpotTheIss::Application.config.secret_token = '8e806cc8e772fcf45b9a31306cb3dc869a642ad6911e16f4d78c105aabc11b31ce82539ec3ce2978be1f359f748260966cc5db6f83cf02bff22d293f523419bf'
